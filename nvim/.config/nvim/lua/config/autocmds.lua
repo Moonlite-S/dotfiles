cb809595d2d0cad3f4220873a6b5a70cd27c2760
Sean <iphoneagents00@gmail.com>
2026-05-07 12:55:46 -0500

    Changed

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -37,3 +37,27 @@ vim.api.nvim_create_autocmd("VimLeave", {
     end
   end,
 })
+
+-- Enforce Strawberry Milk aesthetic for Snacks Picker & Explorer
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    -- Folder / Directory names
+    vim.api.nvim_set_hl(0, "SnacksPickerDir", { fg = "#ffb3c1" })
+
+    -- The vertical tree branches / indent markers
+    vim.api.nvim_set_hl(0, "SnacksPickerTree", { fg = "#4a3036" })
+
+    -- The background color of the currently selected row
+    vim.api.nvim_set_hl(0, "SnacksPickerListCursorLine", { bg = "#3d2b2f" })
+
+    -- The characters that match when you are typing to fuzzy-find
+    vim.api.nvim_set_hl(0, "SnacksPickerMatch", { fg = "#a8e6cf", bold = true })
+
+    -- The title at the top of the explorer/picker window
+    vim.api.nvim_set_hl(0, "SnacksPickerTitle", { fg = "#ffb3c1", bold = true })
+
+    -- Optional: If you want the border of the explorer to be pink too
+    -- vim.api.nvim_set_hl(0, "SnacksPickerBorder", { fg = "#ffb3c1" })
+  end,
+})
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Fix Option + Delete (Alt + Backspace) in Insert Mode
+-- Note: <M-BS> stands for Meta + Backspace
+vim.keymap.set("i", "<M-BS>", "<C-w>", { noremap = true, silent = true })
+-- Some terminals send <M-h> or other sequences, so we cover the most common ones:
+vim.keymap.set("i", "<A-Backspace>", "<C-w>", { noremap = true, silent = true })
