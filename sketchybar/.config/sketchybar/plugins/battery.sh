a3006b1e0e12395985f91ae5fbafc958bc08299d
Sean <iphoneagents00@gmail.com>
2026-05-13 03:07:07 -0500

    Changed some icons around

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -8,21 +8,25 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+9[0-9] | 100)
+  ICON=""
   ;;
-  [6-8][0-9]) ICON=""
+[6-8][0-9])
+  ICON=""
   ;;
-  [3-5][0-9]) ICON=""
+[3-5][0-9])
+  ICON=""
   ;;
-  [1-2][0-9]) ICON=""
+[1-2][0-9])
+  ICON=""
   ;;
-  *) ICON=""
+*) ICON="" ;;
 esac
 
 if [[ "$CHARGING" != "" ]]; then
   ICON=""
 fi
 
-# The item invoking this script (name $NAME) will get its icon and label
+# The item invoking this script (name $NAME) will get its icon
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON"
